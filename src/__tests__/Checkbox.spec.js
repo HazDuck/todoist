a23328ac48389dcd036ff00c73c6b0df3bbaf550
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-04-15 12:40:50 +0100

    set up testing

---
+++ src/__tests__/Checkbox.spec.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import { Checkbox } from '../components/Checkbox'
+
+
+beforeEach(cleanup) //removes any leftover dom elements ==> cleans the DOM!
+
+// mocking = faking a file, function, db so you can test e.g. a fake version of firebase
+
+//there is apparently a nice module to mock firebase for us but here we are doing it manually
+//basically we are returning the firebase object that contains a funciton for each of the 
+//functions that we ue when calling firebase in react - we are just faking these :)
+jest.mock('../firebase', () =>({
+  firebase: {
+    firestore: jest.fn(()=>({
+      collection: jest.fn(()=>({
+        doc: jest.fn(()=> ({
+          update: jest.fn()
+        })),
+      }))
+    })),
+  },
+}))
+
+describe('<Checkbox/>', ()=> {
+  describe('Success', ()=> {
+    it('renders the task checkbox', ()=>{
+      const { queryByTestId, debug } = render(<Checkbox id="1"/>)
+
+      //renders out the full component
+      // debug()
+      
+      expect(queryByTestId('checkbox-action')).toBeTruthy()
+    })
+  })
+  describe('Failure', ()=> {
+
+  })
+})
+
